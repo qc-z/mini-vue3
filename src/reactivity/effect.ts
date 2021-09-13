5c2216c9d843578781252d84b130b99fdf5c9fa4
qichengzhu <18826255448@163.com>
2021-09-13 10:02:14 +0800

    feat: 实现effect返回runner

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -5,7 +5,7 @@ class ReactiveEffect {
   }
   run() {
     activeEffect = this
-    this._fn()
+    return this._fn()
   }
 }
 /**
@@ -38,6 +38,7 @@ let activeEffect
 export function effect(fn) {
   const _effect = new ReactiveEffect(fn)
   _effect.run()
+  return _effect.run.bind(_effect)
 }
 
 export function trigger(target, key) {
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -15,4 +15,16 @@ describe('effect', () => {
     user.age++
     expect(nextAge).toBe(12)
   })
+  it('should bu return runner', () => {
+    // 1 调用effect会返回一个函数(runner)，调用该函数会运行一次effect内部的fn
+    // 2 调用runner会返回fn的返回值
+    let foo = 10
+    const runner = effect(() => {
+      foo++
+      return 'foo'
+    })
+    expect(foo).toBe(11)
+    const r = runner()
+    expect(r).toBe('foo')
+  })
 })
