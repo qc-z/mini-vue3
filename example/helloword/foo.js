55ca3748cef04452a38dfe93b2436ff034e79837
yanyue <yanyue@actionsky.com>
2022-04-22 06:35:47 +0800

    update hello word
    update hello word

--- example/helloword/foo.js
+++ example/helloword/foo.js
@@ -1,14 +1,14 @@
-import { h } from '../../lib/mini-vue.esm.js'
+import { h } from '../../lib/mini-vue.esm.js';
 
 export const Foo = {
   setup(props) {
-    console.log('Foo props', props)
-    props.count++
+    console.log('Foo props show', props);
+    props.count++;
     // 1在setup接收props
     // 在render通过this获取值
     // props必须是shallreadonly
   },
   render() {
-    return h('div', {}, 'foo:' + this.count)
-  }
-}
+    return h('div', {}, 'foo:' + this.count);
+  },
+};
