3d58bb3c23cce4cbc550c2e346197bf1a56cdcaf
yanyue <yanyue@actionsky.com>
2022-04-21 23:06:06 +0800

    UPDATE

--- src/runtime-core/componentEmit.ts
+++ src/runtime-core/componentEmit.ts
@@ -1,13 +1,13 @@
-import { cameliz, toHandlerKey } from '../shared'
+import { toCamel, toHandlerKey } from '../shared';
 
 export function emit(instance, event: string, ...args: any[]) {
   // instance.props --> event
   // 用户要传的是emit(event),instance是无感知的，所以要用到bind
-  const { props } = instance
+  const { props } = instance;
   // TPP,先写一个特例，再写一个通用的
   // add->Add
-  const str = event.charAt(0).toUpperCase() + event.slice(1)
+  const str = event.charAt(0).toUpperCase() + event.slice(1);
 
-  const handler = props[toHandlerKey(cameliz(event))]
-  handler && handler(...args)
+  const handler = props[toHandlerKey(toCamel(event))];
+  handler && handler(...args);
 }
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,25 +1,26 @@
-export const extend = Object.assign
+export const extend = Object.assign;
 
 export const EMPTY_OBJ = {};
 
 export const isObject = (val) => {
-  return val !== null && typeof val === 'object'
-}
+  return val !== null && typeof val === 'object';
+};
 export const hasChanged = (newValue, value) => {
-  return !Object.is(newValue, value)
-}
+  return !Object.is(newValue, value);
+};
 
-export const hasOwn = (val, key) => Object.prototype.hasOwnProperty.call(val, key)
+export const hasOwn = (val, key) =>
+  Object.prototype.hasOwnProperty.call(val, key);
 
 const capitalize = (str: string) => {
-  return str.charAt(0).toUpperCase() + str.slice(1)
-}
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
 export const toHandlerKey = (str: string) => {
-  return str ? 'on' + capitalize(str) : ''
-}
+  return str ? 'on' + capitalize(str) : '';
+};
 // add-foo --> addFoo
-export const cameliz = (str: string) => {
+export const toCamel = (str: string) => {
   return str.replace(/-(\w)/g, (_, c: string) => {
-    return c ? c.toUpperCase() : ''
-  })
-}
+    return c ? c.toUpperCase() : '';
+  });
+};
